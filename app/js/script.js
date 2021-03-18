61380cd9344751d07528cc65dfdb6693d563e48e
thibault <thibault.deverge@gmail.com>
2021-03-18 18:01:09 +0700

    add script

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,8 @@ const rangeInput = document.getElementById("priceRange");
 const btnBilling = document.querySelector(".btnBilling");
 
 /* ----------------------------- Helper function ---------------------------- */
+let costPerMonth = Number(priceView.textContent.slice(1));
+
 const yearlyView = () => {
   btnBilling.classList.add("yearly");
   btnBilling.style.setProperty("--display-ib", "none");
@@ -18,6 +20,18 @@ const monthlyView = () => {
   btnBilling.style.setProperty("--display-none", "none");
 };
 
+const yearlyCalc = () => {
+  costPerMonth = Number(priceView.textContent.slice(1));
+  const costPerYear = costPerMonth * 12 * (75 / 100);
+  priceView.textContent = `$${costPerYear.toFixed(2)}`;
+  priceView.nextSibling.textContent = " / year";
+};
+
+const monthlyCalc = () => {
+  priceView.textContent = `$${costPerMonth.toFixed(0)}.00`;
+  priceView.nextSibling.textContent = " / month";
+};
+
 /* ------------------------- Change Pricing  -------------------------------- */
 
 rangeInput.addEventListener("change", function (e) {
@@ -27,14 +41,23 @@ rangeInput.addEventListener("change", function (e) {
 
   pageView.textContent = pageNbr[rangeValue - 1];
   priceView.textContent = price[rangeValue - 1];
+
+  if (btnBilling.classList.contains("yearly")) {
+    yearlyView();
+    yearlyCalc();
+  } else {
+    pageView.textContent = pageNbr[rangeValue - 1];
+    priceView.textContent = price[rangeValue - 1];
+    monthlyView();
+  }
 });
 
 btnBilling.addEventListener("click", function (e) {
   if (!btnBilling.classList.contains("yearly")) {
     yearlyView();
+    yearlyCalc();
   } else {
     monthlyView();
+    monthlyCalc();
   }
 });
-
-btnBilling.addEventListener("click", function (e) {});
